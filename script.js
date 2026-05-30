9ad4209251148e469ccb8f0e9d6a9c1b7173041d
Vishnupriya1627 <mamillapallivishnupriya@gmail.com>
2026-05-30 17:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here!
 
+const list = document.getElementById("infi-list");
+
+let count = 1;
+
+// Add items to the list
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${count++}`;
+    list.appendChild(li);
+  }
+}
+
+// Add initial 10 items
+addItems(10);
+
+// Infinite scroll
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
